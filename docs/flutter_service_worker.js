f04a5b930993c603bd1b2f5b285eeb5e1f5ead80
GL_ScherrerS <svetlana.scherrer@h-ka.de>
2025-12-04 22:42:40 +0100

    Deploy Flutter Web: 2025-12-04 22:42

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "a53491ba33870e68dce7c820b0867cda
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "143effb069874f27d97787ce5fccfb44",
+"flutter_bootstrap.js": "e73d2fd9e784361c679c49903e83c589",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
