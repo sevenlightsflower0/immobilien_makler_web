b6d57814730ebcf6392adc87638ebe34e555268c
GL_ScherrerS <svetlana.scherrer@h-ka.de>
2025-12-04 22:54:12 +0100

    Deploy Flutter Web: 2025-12-04 22:54

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "716393507"
+    serviceWorkerVersion: "3384349398"
   }
 });
